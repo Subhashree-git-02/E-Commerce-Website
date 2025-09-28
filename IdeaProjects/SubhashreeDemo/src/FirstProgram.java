03128c6a3f09923336d16af36cb1ec5feda4416f
Subhashree Sahoo <subhashreesahoo49021662@gmail.com>
2025-09-28 09:38:48 +0530

    Add Initial Files

---
+++ IdeaProjects/SubhashreeDemo/src/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+    static void main(String args[]) {
+        int a = 10;
+        int b = a * a;
+        System.out.println("Hello World!");
+        System.out.println(b);
+    }
+}
